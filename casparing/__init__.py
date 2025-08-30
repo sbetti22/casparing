5c6725cd58fe7d21fa035a1f97a17e929e185db0
S Betti <42819803+sbetti22@users.noreply.github.com>
2025-08-30 00:25:21 -0400

    Add files via upload

--- casparing/__init__.py
+++ casparing/__init__.py
@@ -3,6 +3,22 @@ __license__ = "MIT"
 __maintainer__ = "Sarah Betti"
 __email__ = "sbetti22@gmail.com"
 
+from .plot import CASPAR_fitMMdot.py
+
 from .derive import CASPAR_astrometry
 from .derive import CASPAR_rederiveaccretion
 from .derive import CASPAR_rederiveparameters
+
+from .plot import CASPAR_plot_MdotAge.py 
+from .plot import CASPAR_plot_variability.py
+from .plot import CASPAR_allskymap.py      
+from .plot import CASPAR_plot_MMdot.py   
+from .plot import CASPAR_plotting.py
+from .plot import CASPAR_cornerplot.py 
+from .plot import CASPAR_plot_MMdotbyDiagnostic.py  
+from .plot import CASPAR_sortdata.py
+    
+from .plot import CASPAR_plot_MMdotbySFR.py  
+from .plot import CASPAR_util.py
+from .plot import CASPAR_Haprofile.py 
+from .plot import CASPAR_plot_physicalParamComparison.py
